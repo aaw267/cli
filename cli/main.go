880d21991f4236d3e7bb8bbb6cd8e77cd73d1ef6
Alex Werner <aaw267@gmail.com>
2026-07-15 01:38:45 +0100

    more

--- cli/main.go
+++ cli/main.go
@@ -3,11 +3,42 @@ package main
 import (
 	"fmt"
 	"os"
-	"strings"
 
-	"github.com/awerner/cli/internal"
+	"github.com/spf13/cobra"
 )
 
+var (
+	uppercase bool
+	lowercase bool
+	titlecase bool
+	wordcount bool
+	charcount bool
+)
+
+var rootCmd = &cobra.Command{
+	Use:   "textfmt [text]",
+	Short: "A simple text formatter",
+	Long: `textfmt is a CLI tool for formatting and analyzing text.
+
+You can provide text as arguments or pipe it in via stdin.
+Multiple formatting options can be applied simultaneously.`,
+	Args: cobra.ArbitraryArgs,
+	Run: func(cmd *cobra.Command, args []string) {
+		fmt.Println("hi!")
+	},
+}
+
+func init() {
+	rootCmd.Flags().BoolVarP(&uppercase, "upper", "u", false, "Convert text to uppercase")
+	rootCmd.Flags().BoolVarP(&lowercase, "lower", "l", false, "Convert text to lowercase")
+	rootCmd.Flags().BoolVarP(&titlecase, "title", "t", false, "Convert text to title case")
+	rootCmd.Flags().BoolVar(&wordcount, "words", false, "Count words in text")
+	rootCmd.Flags().BoolVar(&charcount, "chars", false, "Count characters in text")
+}
+
 func main() {
-	fmt.Println(internal.Echo(strings.Join(os.Args[1:], " ")))
+	if err := rootCmd.Execute(); err != nil {
+		fmt.Println(err)
+		os.Exit(1)
+	}
 }
