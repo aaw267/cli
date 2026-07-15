397d95b3e19d8bc0fb081e53a72d71e266a0ed3f
Alex Werner <aaw267@gmail.com>
2026-07-15 01:16:16 +0100

    asd

--- cli/main.go
+++ cli/main.go
@@ -1,1 +1,13 @@
 package main
+
+import (
+	"fmt"
+	"os"
+	"strings"
+
+	"github.com/awerner/cli/internal"
+)
+
+func main() {
+	fmt.Println(internal.Echo(strings.Join(os.Args[1:], " ")))
+}
---
+++ internal/echo.go
@@ -0,0 +1,5 @@
+package internal
+
+func Echo(in string) string {
+	return in
+}
